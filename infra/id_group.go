387a1baac13611ebaa8092db8cad8cbbf054a1ff
thinkgo <thinkgo@aliyun.com>
2022-06-20 14:50:19 +0800

    chore: use generic

--- infra/id_group.go
+++ infra/id_group.go
@@ -3,66 +3,46 @@ package infra
 import (
 	"strconv"
 	"strings"
+
+	"golang.org/x/exp/constraints"
 )
 
 // ParseGroup 解析以','分隔的整数, 去除0值, 去除重复值
-func ParseGroup(s string) []int64 {
+func ParseGroup[T constraints.Integer](s string) []T {
 	if s == "" {
-		return []int64{}
+		return []T{}
 	}
 
 	ss := strings.Split(s, ",")
-	res := make([]int64, 0, len(ss))
-	mp := make(map[int64]struct{})
+	res := make([]T, 0, len(ss))
+	mp := make(map[T]struct{})
 	for i := 0; i < len(ss); i++ {
 		v, err := strconv.ParseInt(strings.TrimSpace(ss[i]), 0, 64)
 		if err != nil || v == 0 {
 			continue
 		}
-		if _, ok := mp[v]; ok {
+		vv := T(v)
+		if _, ok := mp[vv]; ok {
 			continue
 		}
-		mp[v] = struct{}{}
-		res = append(res, v)
-	}
-	return res
-}
-
-// ParseGroupInt 解析以','分隔的整数, 去除0值, 去除重复值
-func ParseGroupInt(s string) []int {
-	if s == "" {
-		return []int{}
-	}
-
-	ss := strings.Split(s, ",")
-	res := make([]int, 0, len(ss))
-	mp := make(map[int]struct{})
-	for i := 0; i < len(ss); i++ {
-		v, err := strconv.Atoi(strings.TrimSpace(ss[i]))
-		if err != nil || v == 0 {
-			continue
-		}
-		if _, ok := mp[v]; ok {
-			continue
-		}
-		mp[v] = struct{}{}
-		res = append(res, v)
+		mp[vv] = struct{}{}
+		res = append(res, vv)
 	}
 	return res
 }
 
 // JoinGroup 以','分隔的字符串为字符串, 去除0值, 去除重复值
-func JoinGroup(vs []int64) string {
+func JoinGroup[T constraints.Integer](vs []T) string {
 	sep := ","
 	switch len(vs) {
 	case 0:
 		return ""
 	case 1:
-		return strconv.FormatInt(vs[0], 10)
+		return strconv.FormatInt(int64(vs[0]), 10)
 	}
 	strElems := make([]string, 0, len(vs))
 	n := 0
-	mp := make(map[int64]struct{})
+	mp := make(map[T]struct{})
 	for i := 0; i < len(vs); i++ {
 		if vs[i] == 0 {
 			continue
@@ -71,43 +51,7 @@ func JoinGroup(vs []int64) string {
 			continue
 		}
 		mp[vs[i]] = struct{}{}
-		v := strconv.FormatInt(vs[i], 10)
-		strElems = append(strElems, v)
-		n += len(v)
-	}
-	n += len(sep) * (len(strElems) - 1)
-
-	var b strings.Builder
-	b.Grow(n)
-	b.WriteString(strElems[0])
-	for _, s := range strElems[1:] {
-		b.WriteString(sep)
-		b.WriteString(s)
-	}
-	return b.String()
-}
-
-// JoinGroupInt 以','分隔的字符串为字符串, 去除0值, 去除重复值
-func JoinGroupInt(vs []int) string {
-	sep := ","
-	switch len(vs) {
-	case 0:
-		return ""
-	case 1:
-		return strconv.Itoa(vs[0])
-	}
-	strElems := make([]string, 0, len(vs))
-	n := 0
-	mp := make(map[int]struct{})
-	for i := 0; i < len(vs); i++ {
-		if vs[i] == 0 {
-			continue
-		}
-		if _, ok := mp[vs[i]]; ok {
-			continue
-		}
-		mp[vs[i]] = struct{}{}
-		v := strconv.Itoa(vs[i])
+		v := strconv.FormatInt(int64(vs[i]), 10)
 		strElems = append(strElems, v)
 		n += len(v)
 	}
--- infra/id_group_test.go
+++ infra/id_group_test.go
@@ -44,54 +44,8 @@ func TestParseIdsGroup(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := ParseGroup(tt.s); !reflect.DeepEqual(got, tt.want) {
+			if got := ParseGroup[int64](tt.s); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("ParseGroup() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestParseIdsGroupInt(t *testing.T) {
-	tests := []struct {
-		name string
-		s    string
-		want []int
-	}{
-		{
-			"empty",
-			"",
-			[]int{},
-		},
-		{
-			"1",
-			"1",
-			[]int{1},
-		},
-		{
-			"> 1",
-			"1,10,11,12",
-			[]int{1, 10, 11, 12},
-		},
-		{
-			"> 1 contain space",
-			"1, 10, 11 ,  12",
-			[]int{1, 10, 11, 12},
-		},
-		{
-			"> 1 contain dump value",
-			"1, 10, 11 ,  12, 11,1",
-			[]int{1, 10, 11, 12},
-		},
-		{
-			"> 1 contain zero value",
-			"0, 1, 10, 11 ,  12, 0",
-			[]int{1, 10, 11, 12},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := ParseGroupInt(tt.s); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("ParseGroupInt() = %v, want %v", got, tt.want)
 			}
 		})
 	}
@@ -138,52 +92,6 @@ func TestIdsGroup(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := JoinGroup(tt.ids); got != tt.want {
 				t.Errorf("JoinGroup() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestIdsGroupInt(t *testing.T) {
-	tests := []struct {
-		name string
-		ids  []int
-		want string
-	}{
-		{
-			"empty nil",
-			nil,
-			"",
-		},
-		{
-			"empty",
-			[]int{},
-			"",
-		},
-		{
-			"1",
-			[]int{1},
-			"1",
-		},
-		{
-			"> 1",
-			[]int{1, 10, 11, 12},
-			"1,10,11,12",
-		},
-		{
-			"> 1 contain dump value",
-			[]int{1, 10, 11, 11, 12, 1},
-			"1,10,11,12",
-		},
-		{
-			"> 1 contain zero/dump value",
-			[]int{0, 1, 10, 0, 11, 11, 0, 12},
-			"1,10,11,12",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := JoinGroupInt(tt.ids); got != tt.want {
-				t.Errorf("JoinGroupInt() = %v, want %v", got, tt.want)
 			}
 		})
 	}
