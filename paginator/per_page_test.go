4a7c84a09127b7c81527e32a6c813f9426b9a240
thinkgo <thinkgo@aliyun.com>
2022-08-24 10:34:49 +0800

    fix: fix paginator bug

--- paginator/per_page_test.go
+++ paginator/per_page_test.go
@@ -6,11 +6,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParam(t *testing.T) {
+func TestParam2(t *testing.T) {
 	param := Param2{}
 	assert.Equal(t, Param2{1, DefaultPageSize}, param.Inspect())
 
-	assert.Equal(t, Param2{1, DefaultMaxPageSize}, New(0, 20000))
+	assert.Equal(t, Param2{1, DefaultMaxPageSize}, New2(0, 20000))
 
 	param = Param2{}
 	assert.EqualValues(t, Param2{1, 20}, param.Inspect(20))
